d82a589088049d69df0d7d7b94704c8f8ef3ed0f
lucid-dreem <59326141+lucid-dreem@users.noreply.github.com>
2020-09-05 20:08:54 +0530

    teacher panel

---
+++ coding-monks-teacher/assets/js/discussion.js
@@ -0,0 +1,17 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+      const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+
+
+      accordionItemHeader.classList.toggle("active");
+      const accordionItemBody = accordionItemHeader.nextElementSibling;
+      if (accordionItemHeader.classList.contains("active")) {
+        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+      }
+      else{
+        accordionItemBody.style.maxHeight = 0;
+      }
+    });
+});
