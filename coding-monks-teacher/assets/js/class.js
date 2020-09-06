de360dd21e00ac0ac060f976339fc70933df9f2e
Lucid Dream <59326141+lucid-dreem@users.noreply.github.com>
2020-09-06 08:30:31 +0530

    minor fixes, about, attendance

--- coding-monks-teacher/assets/js/class.js
+++ coding-monks-teacher/assets/js/class.js
@@ -1,28 +1,21 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  $(document).ready(function(){
-elem = $(".elem");
-elem.mouseup(function(event) {
-    if ($(this).children("input").length == 0) {
-        var old = $(this).html();
-        $(this).empty();
-        var input = $('<input type=text>');
-        input.val(old);
-        $(this).append(input);      
-        input.select();
-        input.blur(function() {
-            var value = $(this).val();
-            if (value !== "") {
-                $(this).parent().html($(this).val());
-            } else {
-                $(this).parent().html("Text");
-            }
-        });
-    }
-});
-});
+$(document).ready(function(){
+    elem = $(".elem");
+    elem.mouseup(function(event) {
+        if ($(this).children("input").length == 0) {
+            var old = $(this).html();
+            $(this).empty();
+            var input = $('<input type=text>');
+            input.val(old);
+            $(this).append(input);      
+            input.select();
+            input.blur(function() {
+                var value = $(this).val();
+                if (value !== "") {
+                    $(this).parent().html($(this).val());
+                } else {
+                    $(this).parent().html("Text");
+                }
+            });
+        }
+    });
+    });
